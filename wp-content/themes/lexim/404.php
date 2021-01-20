b06cd7abd6b3b802b8e5b0fefc0091dce8002e5f
hiep <hiep.dao@leximit.com>
2021-01-20 11:33:13 +0700

    404 page, GPDR

--- wp-content/themes/lexim/404.php
+++ wp-content/themes/lexim/404.php
@@ -10,51 +10,16 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+    <div id="main" class="site-main page-main" role="main">
 
-		<section class="error-404 not-found">
+		<section class="error-404-page not-found one_col">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'lexim' ); ?></h1>
-			</header><!-- .page-header -->
+                <p class="hncHeading bold-font">404</p>
+				<h1 class="page-title">Oops! That page can't be found.</h1>
+			</header>
+		</section>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'lexim' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'lexim' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$lexim_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'lexim' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$lexim_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+    </div>
 
 <?php
 get_footer();
